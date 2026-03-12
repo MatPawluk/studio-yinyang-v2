4037644bedf6f2d8a7787eefece185b667c0aa5f
MatPawluk <pawluk1988@icloud.com>
2026-03-12 15:26:40 +0100

    style: rotate 3D logo 180deg in X-axis on load

--- src/components/ui/YinYangLogo3D.tsx
+++ src/components/ui/YinYangLogo3D.tsx
@@ -13,7 +13,7 @@ function Model({ url }: { url: string }) {
     }
   });
 
-  return <primitive ref={modelRef} object={scene} scale={2.5} />;
+  return <primitive ref={modelRef} object={scene} scale={2.5} rotation={[Math.PI, 0, 0]} />;
 }
 
 export function YinYangLogo3D() {
